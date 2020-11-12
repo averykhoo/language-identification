839b8ed4e83d395daf72b286c95842fcaa73e0af
averykhoo <averykhoo@gmail.com>
2020-11-12 23:00:19 +0800

    FIXED DESERET BOOK

--- deseret/deseret_conversion.py
+++ deseret/deseret_conversion.py
@@ -1,170 +1,40 @@
 deseret = {
-    'Ä': '𐐀', '': '𐐨',  # LONG I (i as in machine)
-    'Å': '𐐁', '': '𐐩',  # LONG E (e as in grey)
-    'Ç': '𐐂', '': '𐐪',  # LONG A (a as in art)
-    'É': '𐐃', '': '𐐫',  # LONG AH (au as in aught)
-    'Ñ': '𐐄', '': '𐐬',  # LONG O (o as in tone)
-    'Ö': '𐐅', '': '𐐭',  # LONG OO (oo as in moo)
+    'Ä': '𐐀', '¥': '𐐨',  # LONG I (i as in machine)
+    'Å': '𐐁', '∂': '𐐩',  # LONG E (e as in grey)
+    'Ç': '𐐂', '∑': '𐐪',  # LONG A (a as in art)
+    'É': '𐐃', '∏': '𐐫',  # LONG AH (au as in aught)
+    'Ñ': '𐐄', 'π': '𐐬',  # LONG O (o as in tone)
+    'Ö': '𐐅', '∫': '𐐭',  # LONG OO (oo as in moo)
     'Ü': '𐐆', 'ª': '𐐮',  # SHORT I (i as in it)
-    'á': '𐐇', '': '𐐯',  # SHORT E (e as in desert)
-    'à': '𐐈', '': '𐐰',  # SHORT A (a as in cat)
-    'â': '𐐉', '': '𐐱',  # SHORT AH (o as in cot)
+    'á': '𐐇', 'º': '𐐯',  # SHORT E (e as in desert)
+    'à': '𐐈', 'Ω': '𐐰',  # SHORT A (a as in cat)
+    'â': '𐐉', 'æ': '𐐱',  # SHORT AH (o as in cot)
     'ä': '𐐊', 'ø': '𐐲',  # SHORT O (o as in woman)
-    'ã': '𐐋', '': '𐐳',  # SHORT OO (oo as in book)
-    'å': '𐐌', '': '𐐴',  # AY (the sound of the pronoun I)
+    'ã': '𐐋', '¿': '𐐳',  # SHORT OO (oo as in book)
+    'å': '𐐌', '¡': '𐐴',  # AY (the sound of the pronoun I)
     'ç': '𐐍', 'À': '𐐵',  # OW (ow as in cow)
-    'é': '𐐎', '': '𐐶',  # WU (w as in wonder)
-    'è': '𐐏', '': '𐐷',  # YEE (y as in you)
-    'ê': '𐐐', '': '𐐸',  # H (h as in hurrah)
-    'ë': '𐐑', '': '𐐹',  # PEE (p as in put)
+    'é': '𐐎', 'Ã': '𐐶',  # WU (w as in wonder)
+    'è': '𐐏', 'Õ': '𐐷',  # YEE (y as in you)
+    'ê': '𐐐', 'Œ': '𐐸',  # H (h as in hurrah)
+    'ë': '𐐑', 'œ': '𐐹',  # PEE (p as in put)
     'í': '𐐒', 'ÿ': '𐐺',  # BEE (b as in bee)
     'ì': '𐐓', 'Ÿ': '𐐻',  # TEE (t as in teach)
     'î': '𐐔', 'ﬁ': '𐐼',  # DEE (d as in Deseret)
-    'ï': '𐐕', '': '𐐽',  # CHEE (ch as in cheese)
-    'ñ': '𐐖', '': '𐐾',  # JEE (j as in John)
-    'ó': '𐐗', '': '𐐿',  # KAY (k as in kitten)
-    'ò': '𐐘', '': '𐑀',  # GAY (g as in good)
-    'ô': '𐐙', '': '𐑁',  # EF (f as in France)
-    'ö': '𐐚', '': '𐑂',  # VEE (v as in voice)
-    'õ': '𐐛', '': '𐑃',  # ETH (unvoiced th as in think)
+    'ï': '𐐕', 'ﬂ': '𐐽',  # CHEE (ch as in cheese)
+    'ñ': '𐐖', 'Â': '𐐾',  # JEE (j as in John)
+    'ó': '𐐗', 'Ê': '𐐿',  # KAY (k as in kitten)
+    'ò': '𐐘', 'Á': '𐑀',  # GAY (g as in good)
+    'ô': '𐐙', 'Ë': '𐑁',  # EF (f as in France)
+    'ö': '𐐚', 'È': '𐑂',  # VEE (v as in voice)
+    'õ': '𐐛', 'Í': '𐑃',  # ETH (unvoiced th as in think)
     'ú': '𐐜', 'Î': '𐑄',  # THEE (voiced th as in they)
     'ù': '𐐝', 'Ï': '𐑅',  # ES (s as in said)
-    'û': '𐐞', '': '𐑆',  # ZEE (z as in zebra)
+    'û': '𐐞', 'Ì': '𐑆',  # ZEE (z as in zebra)
     'ü': '𐐟', 'Ó': '𐑇',  # ESH (sh as in shoot)
-    '¢': '𐐠', '': '𐑈',  # ZHEE (as in measure)
-    'ß': '𐐡', '': '𐑉',  # ER (r as in ride)
-    '´': '𐐢', '': '𐑊',  # EL (l as in letter)
-    '¨': '𐐣', '': '𐑋',  # EM (m as in Macintosh)
-    'Æ': '𐐤', '': '𐑌',  # EN (n as in nice)
-    'Ø': '𐐥', '': '𐑍',  # ENG (ng as in sing)
+    '¢': '𐐠', 'Ô': '𐑈',  # ZHEE (as in measure)
+    'ß': '𐐡', 'Ò': '𐑉',  # ER (r as in ride)
+    '´': '𐐢', 'Ú': '𐑊',  # EL (l as in letter)
+    '¨': '𐐣', 'Û': '𐑋',  # EM (m as in Macintosh)
+    'Æ': '𐐤', 'Ù': '𐑌',  # EN (n as in nice)
+    'Ø': '𐐥', 'ı': '𐑍',  # ENG (ng as in sing)
 }
-
-lowercase = {
-    'Ä': '𐐨',
-    'Å': '𐐩',
-    'Ç': '𐐪',
-    'É': '𐐫',
-    'Ñ': '𐐬',
-    'Ö': '𐐭',
-    'Ü': '𐐮',
-    'á': '𐐯',
-    'à': '𐐰',
-    'â': '𐐱',
-    'ä': '𐐲',
-    'ã': '𐐳',
-    'å': '𐐴',
-    'ç': '𐐵',
-    'é': '𐐶',
-    'è': '𐐷',
-    'ê': '𐐸',
-    'ë': '𐐹',
-    'í': '𐐺',
-    'ì': '𐐻',
-    'î': '𐐼',
-    'ï': '𐐽',
-    'ñ': '𐐾',
-    'ó': '𐐿',
-    'ò': '𐑀',
-    'ô': '𐑁',
-    'ö': '𐑂',
-    'õ': '𐑃',
-    'ú': '𐑄',
-    'ù': '𐑅',
-    'û': '𐑆',
-    'ü': '𐑇',
-    '¢': '𐑈',
-    'ß': '𐑉',
-    '´': '𐑊',
-    '¨': '𐑋',
-    'Æ': '𐑌',
-    'Ø': '𐑍',
-}
-
-deseret = '''
-U+10400 Ä U+10428     LONG I (i as in machine)             DESERET CAPITAL LETTER LONG I  
-U+10401 Å U+10429     LONG E (e as in grey)                DESERET CAPITAL LETTER LONG E  
-U+10402 Ç U+1042A     LONG A (a as in art)                 DESERET CAPITAL LETTER LONG A  
-U+10403 É U+1042B Π    LONG AH (au as in aught)             DESERET CAPITAL LETTER LONG AH  
-U+10404 Ñ U+1042C     LONG O (o as in tone)                DESERET CAPITAL LETTER LONG O  
-U+10405 Ö U+1042D     LONG OO (oo as in moo)               DESERET CAPITAL LETTER LONG OO  
-U+10406 Ü U+1042E     SHORT I (i as in it)                 DESERET CAPITAL LETTER SHORT I  
-U+10407 á U+1042F     SHORT E (e as in desert)             DESERET CAPITAL LETTER SHORT E  
-U+10408 à U+10430     SHORT A (a as in cat)                DESERET CAPITAL LETTER SHORT A  
-U+10409 â U+10431     SHORT AH (o as in cot)               DESERET CAPITAL LETTER SHORT AH  
-U+1040A ä U+10432 ø    SHORT O (o as in woman)              DESERET CAPITAL LETTER SHORT O  
-U+1040B ã U+10433 ¿    SHORT OO (oo as in book)             DESERET CAPITAL LETTER SHORT OO  
-U+1040C å U+10434     AY (the sound of the pronoun I)      DESERET CAPITAL LETTER AY  
-U+1040D ç U+10435     OW (ow as in cow)                    DESERET CAPITAL LETTER OW  
-U+1040E é U+10436     WU (w as in wonder)                  DESERET CAPITAL LETTER WU  
-U+1040F è U+10437     YEE (y as in you)                    DESERET CAPITAL LETTER YEE  
-U+10410 ê U+10438     H (h as in hurrah)                   DESERET CAPITAL LETTER H  
-U+10411 ë U+10439     PEE (p as in put)                    DESERET CAPITAL LETTER PEE  
-U+10412 í U+1043A     BEE (b as in bee)                    DESERET CAPITAL LETTER BEE  
-U+10413 ì U+1043B     TEE (t as in teach)                  DESERET CAPITAL LETTER TEE  
-U+10414 î U+1043C     DEE (d as in Deseret)                DESERET CAPITAL LETTER DEE  
-U+10415 ï U+1043D     CHEE (ch as in cheese)               DESERET CAPITAL LETTER CHEE  
-U+10416 ñ U+1043E     JEE (j as in John)                   DESERET CAPITAL LETTER JEE  
-U+10417 ó U+1043F Ê    KAY (k as in kitten)                 DESERET CAPITAL LETTER KAY  
-U+10418 ò U+10440     GAY (g as in good)                   DESERET CAPITAL LETTER GAY  
-U+10419 ô U+10441     EF (f as in France)                  DESERET CAPITAL LETTER EF  
-U+1041A ö U+10442 È    VEE (v as in voice)                  DESERET CAPITAL LETTER VEE  
-U+1041B õ U+10443     ETH (unvoiced th as in think)        DESERET CAPITAL LETTER ETH  
-U+1041C ú U+10444     THEE (voiced th as in they)          DESERET CAPITAL LETTER THEE  
-U+1041D ù U+10445     ES (s as in said)                    DESERET CAPITAL LETTER ES  
-U+1041E û U+10446     ZEE (z as in zebra)                  DESERET CAPITAL LETTER ZEE  
-U+1041F ü U+10447     ESH (sh as in shoot)                 DESERET CAPITAL LETTER ESH  
-U+10420 ¢ U+10448     ZHEE (as in measure)                 DESERET CAPITAL LETTER ZHEE  
-U+10421 ß U+10449 ñ    ER (r as in ride)                    DESERET CAPITAL LETTER ER  
-U+10422 ´ U+1044A     EL (l as in letter)                  DESERET CAPITAL LETTER EL  
-U+10423 ¨ U+1044B ó    EM (m as in Macintosh)               DESERET CAPITAL LETTER EM  
-U+10424 Æ U+1044C ô    EN (n as in nice)                    DESERET CAPITAL LETTER EN  
-U+10425 Ø U+1044D     ENG (ng as in sing)                  DESERET CAPITAL LETTER ENG  
-'''.strip()
-'''
-U+10428                              DESERET SMALL LETTER LONG I
-U+10429                              DESERET SMALL LETTER LONG E
-U+1042A                              DESERET SMALL LETTER LONG A
-U+1042B                              DESERET SMALL LETTER LONG AH
-U+1042C                              DESERET SMALL LETTER LONG O
-U+1042D                              DESERET SMALL LETTER LONG OO
-U+1042E                              DESERET SMALL LETTER SHORT I
-U+1042F                              DESERET SMALL LETTER SHORT E
-U+10430                              DESERET SMALL LETTER SHORT A
-U+10431                              DESERET SMALL LETTER SHORT AH
-U+10432                              DESERET SMALL LETTER SHORT O
-U+10433                              DESERET SMALL LETTER SHORT OO
-U+10434                              DESERET SMALL LETTER AY
-U+10435                              DESERET SMALL LETTER OW
-U+10436                              DESERET SMALL LETTER WU
-U+10437                              DESERET SMALL LETTER YEE
-U+10438                              DESERET SMALL LETTER H
-U+10439                              DESERET SMALL LETTER PEE
-U+1043A                              DESERET SMALL LETTER BEE
-U+1043B                              DESERET SMALL LETTER TEE
-U+1043C                              DESERET SMALL LETTER DEE
-U+1043D                              DESERET SMALL LETTER CHEE
-U+1043E                              DESERET SMALL LETTER JEE
-U+1043F                              DESERET SMALL LETTER KAY
-U+10440                              DESERET SMALL LETTER GAY
-U+10441                              DESERET SMALL LETTER EF
-U+10442                              DESERET SMALL LETTER VEE
-U+10443                              DESERET SMALL LETTER ETH
-U+10444                              DESERET SMALL LETTER THEE
-U+10445                              DESERET SMALL LETTER ES
-U+10446                              DESERET SMALL LETTER ZEE
-U+10447                              DESERET SMALL LETTER ESH
-U+10448                              DESERET SMALL LETTER ZHEE
-U+10449                              DESERET SMALL LETTER ER
-U+1044A                              DESERET SMALL LETTER EL
-U+1044B                              DESERET SMALL LETTER EM
-U+1044C                              DESERET SMALL LETTER EN
-U+1044D                              DESERET SMALL LETTER ENG
-      
-'''
-
-if __name__ == '__main__':
-    out = dict()
-    for line in deseret.split('\n'):
-        uni_up, char_up, uni_down, char_down, *_ = line.split(' ')
-        out[char_up] = chr(int(uni_down[2:], 16))
-    print(out)
